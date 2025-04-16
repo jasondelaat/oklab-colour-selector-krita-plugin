9df039b519bcd632ad50658ea6c26897be3dd473
Jason DeLaat <jasondelaat@users.noreply.github.com>
2025-04-16 07:53:52 -0400

    Implemented hue slider which actually displays hues.

--- lab_colour_picker/hue_plane_selector.py
+++ lab_colour_picker/hue_plane_selector.py
@@ -15,6 +15,39 @@ from krita import *
 from .selector_common import *
 from .util import *
 
+class HueEffect(QGraphicsEffect):
+    def __init__(self, parent):
+        super().__init__(parent)
+        data = []
+        for y in range(10):
+            for x in range(255):
+                hue = 2 * (x/255) * math.pi
+                a = 0.085*math.cos(hue)
+                b = 0.085*math.sin(hue)
+                lab = Lab(0.5, a, b)
+                rgb, _ = oklab_to_srgb(lab)
+                c = QColor(rgb[0], rgb[1], rgb[2]).toHsl()
+                hue = c.hslHueF()
+                c.setHslF(hue, 1, 0.5)
+                c = c.toRgb()
+                data.append(c.red())
+                data.append(c.green())
+                data.append(c.blue())
+                data.append(255)
+
+        im = QImage(bytes(data), 255, 10, QImage.Format_RGBA8888)
+        self.pixmap = QPixmap.fromImage(im)
+        self.arrow = Krita.instance().icon('docker_collapse_a').pixmap(16, 16)
+        self.arrow = self.arrow.transformed(QTransform(1, 0, 0, -1, 0, 16))
+        
+    def draw(self, painter):
+        width = self.parent().width()
+        val = self.parent().value()
+        painter.drawPixmap(QRectF(10, -5, width-20, 10), self.pixmap, QRectF(0, 0, 255, 10))
+        painter.drawPixmap(QPoint(2 + int(val*(width-20)/1000), 0), self.arrow)
+        #self.drawSource(painter)
+
+
 class OKLabHuePlane(SelectorSurface):
     def __init__(self, parent, display_size):
         super().__init__(parent, display_size)
@@ -65,6 +98,7 @@ class OKLabHuePlaneSelector(QWidget):
         self.setLayout(layout)
 
         self.hue = Slider(self, 'Hue:', 0, self.modifyHue)
+        self.hue.slider.setGraphicsEffect(HueEffect(self.hue.slider))
         self.plane = OKLabHuePlane(self, 256)
         layout.addWidget(self.hue)
         layout.addWidget(self.plane)
